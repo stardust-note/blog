14ff1445b2ba867c03e80eed87fb8d327ddce743
stardust <stardust.official.kr@gmail.com>
2025-11-09 13:47:19 +0900

    pagination art

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 export default function Pagination({ currentPage, totalPages, onPageChange }) {
   return (
-    <div className="flex justify-center items-center space-x-4 mt-12">
+    <div className="flex justify-center items-center space-x-1 mt-12 text-sm">
       {/* 이전 버튼 */}
       {currentPage > 1 && (
         <button
           onClick={() => onPageChange(currentPage - 1)}
-          className="px-4 py-2 rounded-full bg-gray-100 text-gray-700 hover:bg-gray-200 transition"
+          className="px-2 py-1 text-gray-700 cursor-pointer"
         >
           이전
         </button>
@@ -17,30 +17,34 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
 
       {/* 페이지 번호 */}
       <div className="flex space-x-2">
-        {Array.from({ length: totalPages }, (_, i) => (
-          <button
-            key={i}
-            onClick={() => onPageChange(i + 1)}
-            className={`px-4 py-2 rounded-full font-medium transition ${
-              currentPage === i + 1
-                ? 'bg-gradient-to-r from-purple-500 to-pink-500 text-white'
-                : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
-            }`}
-          >
-            {i + 1}
-          </button>
-        ))}
+        {Array.from({ length: totalPages }, (_, i) => {
+          const pageNum = String(i + 1).padStart(2, "0");
+          return (
+            <button
+              key={i}
+              onClick={() => onPageChange(i + 1)}
+              className={`py-1 mx-2 transition text-xs ${
+                currentPage === i + 1
+                  ? 'color-[#2d2d2d] font-black border-b-2 border-[#2d2d2d]'
+                  : 'text-gray-300 cursor-pointer hover:bg-gray-200'
+              }`}
+            >
+              {pageNum}
+            </button>
+          );
+        })}
       </div>
 
       {/* 다음 버튼 */}
       {currentPage < totalPages && (
         <button
           onClick={() => onPageChange(currentPage + 1)}
-          className="px-4 py-2 rounded-full bg-gray-100 text-gray-700 hover:bg-gray-200 transition"
+          className="px-2 py-1  text-gray-700 cursor-pointer"
         >
           다음
         </button>
       )}
     </div>
+
   )
 }
