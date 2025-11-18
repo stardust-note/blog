d41cc3481e729d5ed2fdb950db22af68701ab580
stardust <stardust.official.kr@gmail.com>
2025-11-18 21:34:31 +0900

    scroll save del

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css'
 import { Noto_Sans_KR } from 'next/font/google'
 import Layout from '../components/Layout'
-import ScrollRestoration from "../components/ScrollRestoration";
 
 const notoSans = Noto_Sans_KR({
   weight: ["300", "400", "500", "700"],
@@ -19,7 +18,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko" className={notoSans.className}>
       <body className="min-h-screen bg-gradient-to-br from-slate-50 via-white to-blue-50 ">
-        <ScrollRestoration />
         <Layout>
           {children}
         </Layout>
